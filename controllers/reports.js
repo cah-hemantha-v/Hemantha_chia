ceac57090bf2571489035b49786712d1a03ed690
Bellamkonda <sai.bellamkonda@cardinalhealth.com>
2019-07-17 08:18:03 -0400

    Remaining reports to insert to db

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,6 +1,10 @@
+const sql = require('mssql');
 const watson = require('./watson');
 const iPrice = require('./iPrice');
 const logger = require('../utils/logger');
+const ServiceNow = require('./snowhelper');
+const dbConfig = require('./db');
+
 module.exports = class Reports {
     constructor() {
         this.iprice = new iPrice();
@@ -63,6 +67,42 @@ module.exports = class Reports {
                 this.watson.watsonPostMessage(this.watson.response).then((rest) => {
                     resolve(rest);
                 });
+            });
+        });
+    }
+
+    submitReportRequest(uid) {
+        logger.debug("Submit Report request Code called...");
+        return new Promise((resolve, reject) => {
+            this.watson.setContext("Submit_Report", false);
+            ServiceNow.getUserProfile(uid).then((result) => {
+                let userEmail = result[0].email;
+                let Source = 'CHIA';
+                let sR = this.watson.getContext("submitreport");
+                sql.connect(dbConfig).then(() => {
+                    return sql.query `insert into Chatbot (UniqueID,Source,Requestor,Timestamp,ReportType,Soldto,ContractTypes,
+                        GPO,UsageTimeframe,ReportStartDate,ReportEndDate,RequestText) values 
+                  (${sR.UniqueID},${Source},${userEmail},${sR.Timestamp},${sR.ReportType},${sR.Soldto},${sR.ContractTypes},${sR.GPO},
+                   ${sR.UsageTimeframe},${sR.ReportStartDate},${sR.ReportEndDate},${sR.RequestText})`
+                }).then((dbResultSet) => {
+                    console.log(dbResultSet);
+                    this.watson.setContext("isReportSubmitted", true);
+                    this.watson.watsonPostMessage(this.watson.response).then((rest) => {
+                        resolve(rest);
+                    });
+                    sql.close();
+                }).catch(err => {
+                    // ... error checks
+                    console.log(err)
+                    sql.close();
+                    this.watson.setContext("submitreporterr", true);
+                    this.watson.watsonPostMessage(this.watson.response).then((rest) => {
+                        resolve(rest);
+                    });
+                })
+            }).catch((err) => {
+                logger.error(err);
+                reject(err);
             });
         });
     }
