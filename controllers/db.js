44138a1f3d954e05cd109cb5cea24661d912ed60
Bellamkonda <sai.bellamkonda@cardinalhealth.com>
2019-10-16 11:43:54 -0400

    Credit&Rebill watson fix

--- controllers/db.js
+++ controllers/db.js
@@ -2,7 +2,8 @@ const dbConfig = {
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST,
-    database: 'ContractsAndPricing'
+    database: 'ContractsAndPricing',
+    requestTimeout: 30000
 };
 
 module.exports = dbConfig; 
